7995d108ade6c38ef6103766dd744d8f412dc357
VitalyCloud <vitalicpavlenco@gmail.com>
2019-11-16 20:23:50 +0300

    Initial Commit

---
+++ Matrix/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Matrix
+//
+//  Created by Виталий Павленко on 16/11/2019.
+//  Copyright © 2019 Виталий Павленко. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
